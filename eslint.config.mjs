19bc1ff70a5a522f26456ff15e3e9ae9934da7b4
Coffee☕️ <coffee@coffeebot.dev>
2024-09-10 15:13:12 -0400

    enable strict but warn; ci lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,15 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.stylistic,
+  ...tseslint.configs.strict,
   {
     plugins: {
       'simple-import-sort': eslintPluginSimpleImportSort
     },
     rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
       '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn', // or "error"

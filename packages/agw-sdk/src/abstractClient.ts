4347f6cba93ec650681fdbc087c677f623040d66
cygaar <97691933+cygaar@users.noreply.github.com>
2024-09-16 19:03:38 -0400

    Use http transport for public client (#8)

--- packages/agw-sdk/src/abstractClient.ts
+++ packages/agw-sdk/src/abstractClient.ts
@@ -57,7 +57,7 @@ export async function createAbstractClient({
 
   const publicClient = createPublicClient({
     chain: chain,
-    transport,
+    transport: http(),
   });
 
   const smartAccountAddress = await getSmartAccountAddressFromInitialSigner(

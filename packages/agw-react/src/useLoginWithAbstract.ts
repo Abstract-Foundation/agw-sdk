6222364c6db2c9376c42519f1c8536ff9a959f1c
Coffee☕️ <coffee@coffeebot.dev>
2024-09-16 12:34:42 -0400

    add agw-react package

---
+++ packages/agw-react/src/useLoginWithAbstract.ts
@@ -0,0 +1,42 @@
+import { useCrossAppAccounts, usePrivy, type User } from '@privy-io/react-auth';
+import { useCallback } from 'react';
+
+const AGW_APP_ID = 'cm04asygd041fmry9zmcyn5o5';
+
+interface AbstractGlobalWalletInterface {
+  /** Boolean to indicate whether the abstract global wallet state has initialized */
+  ready: boolean;
+  /** Boolean to indicate whether the user is authenticated */
+  authenticated: boolean;
+  /** Privy user object */
+  user: User | undefined;
+  /** Function to login with the Abstract global wallet */
+  login: () => Promise<void>;
+  /** Function to logout of the abstract global wallet */
+  logout: () => Promise<void>;
+}
+
+export const useLoginWithAbstract = (): AbstractGlobalWalletInterface => {
+  const { loginWithCrossAppAccount } = useCrossAppAccounts();
+  const { user, ready, authenticated, logout } = usePrivy();
+
+  const login = useCallback(async () => {
+    if (!ready) return;
+    if (!authenticated) {
+      try {
+        await loginWithCrossAppAccount({ appId: AGW_APP_ID });
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+    }
+  }, [ready, authenticated, loginWithCrossAppAccount]);
+
+  return {
+    ready,
+    authenticated,
+    user: user ?? undefined,
+    login,
+    logout,
+  };
+};

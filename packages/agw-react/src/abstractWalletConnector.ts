7cb04bc88827a0a859c3b035588b89d1cb7596b5
Coffee☕️ <coffee@coffeebot.dev>
2024-09-17 16:41:40 -0400

    update viem peer version; tweak wallet connectors

--- packages/agw-react/src/abstractWalletConnector.ts
+++ packages/agw-react/src/abstractWalletConnector.ts
@@ -8,7 +8,32 @@ import {
 
 import { AGW_APP_ID } from './constants.js';
 
-export function createAbstractWalletConnector(): CreateConnectorFn<
+/**
+ * Create a wagmi connector for the Abstract Global Wallet.
+ *
+ * Adapted from wagmi injected connector as a reference implementation:
+ * https://github.com/wevm/wagmi/blob/main/packages/core/src/connectors/injected.ts#L94
+ *
+ * @example
+ * import { createConfig, http } from "wagmi";
+ * import { abstract } from "wagmi/chains";
+ *
+ * const privyWalletConnector = toPrivyWalletConnector({
+ *   providerAppId: <your-app-id>,
+ *   providerName: 'Your app',
+ *   providerIconUrl: 'https://example.com/image.png',
+ * })
+ *
+ * export const wagmiConfig = createConfig({
+ *   chains: [mainnet],
+ *   transports: {
+ *     [mainnet.id]: http(),
+ *   },
+ *   connectors: [abstractWalletConnector],
+ *   ssr: true,
+ * });
+ */
+function abstractWalletConnector(): CreateConnectorFn<
   {
     on: <event extends keyof EIP1193EventMap>(
       event: event,
@@ -72,3 +97,5 @@ export function createAbstractWalletConnector(): CreateConnectorFn<
     return abstractConnector;
   };
 }
+
+export { abstractWalletConnector };

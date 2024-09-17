d76ad946bfbe8d27c09835d8830fe9142d4fc65d
cygaar <tmpftl1@gmail.com>
2024-09-17 19:33:58 -0400

    Update include in vitest config

--- packages/agw-sdk/test/vitest.config.ts
+++ packages/agw-sdk/test/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
+    include: [join(__dirname, './src/**/*.test.ts')],
     globalSetup: [join(__dirname, './globalSetup.ts')],
   },
 });

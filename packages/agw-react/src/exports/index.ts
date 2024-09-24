97b1eda3a38bd54bc98d9d98b32681207bb372f1
Coffee☕️ <coffee@coffeebot.dev>
2024-09-24 10:46:14 -0400

    make wagmi peer dep

--- packages/agw-react/src/exports/index.ts
+++ packages/agw-react/src/exports/index.ts
@@ -1,3 +1,1 @@
 export { AbstractWalletProvider } from '../agwProvider.js';
-export { useAbstractGlobalWallet } from '../smartAccountContext.js';
-export { useLoginWithAbstract } from '../useLoginWithAbstract.js';

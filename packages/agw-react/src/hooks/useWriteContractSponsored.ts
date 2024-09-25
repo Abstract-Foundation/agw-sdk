f7e60c7961aaa0b06d1a6598241b871c1e8d96bc
Coffee☕️ <coffee@coffeebot.dev>
2024-09-25 08:49:44 -0400

    refactor & fix typing

--- packages/agw-react/src/hooks/useWriteContractSponsored.ts
+++ packages/agw-react/src/hooks/useWriteContractSponsored.ts
@@ -1,25 +1,14 @@
-import type { MutateOptions, MutationOptions } from '@tanstack/query-core';
+import type { MutationOptions } from '@tanstack/query-core';
 import { writeContract } from '@wagmi/core';
+import type { Abi, WriteContractErrorType } from 'viem';
+import { type Config, type ResolvedRegister, useConfig } from 'wagmi';
+import { useMutation, type WriteContractData } from 'wagmi/query';
+
 import type {
-  Abi,
-  Address,
-  ContractFunctionArgs,
-  ContractFunctionName,
-  Hex,
-  WriteContractErrorType,
-} from 'viem';
-import {
-  type Config,
-  type ResolvedRegister,
-  useConfig,
-  type UseWriteContractParameters,
-} from 'wagmi';
-import {
-  useMutation,
-  type UseMutationReturnType,
-  type WriteContractData,
-  type WriteContractVariables,
-} from 'wagmi/query';
+  UseWriteContractSponsoredParameters,
+  UseWriteContractSponsoredReturnType,
+  WriteContractSponsoredVariables,
+} from '../actions/writeContractSponsored.js';
 
 export function writeContractSponsoredMutationOptions<config extends Config>(
   config: config,
@@ -32,133 +21,15 @@ export function writeContractSponsoredMutationOptions<config extends Config>(
   } as const satisfies MutationOptions<
     WriteContractData,
     WriteContractErrorType,
-    WriteContractVariables<
+    WriteContractSponsoredVariables<
       Abi,
       string,
       readonly unknown[],
       config,
       config['chains'][number]['id']
-    > & {
-      paymaster: Address;
-      paymasterInput: Hex;
-    }
+    >
   >;
 }
-
-export type WriteContractSponsoredMutate<
-  config extends Config,
-  context = unknown,
-> = <
-  const abi extends Abi | readonly unknown[],
-  functionName extends ContractFunctionName<abi, 'nonpayable' | 'payable'>,
-  args extends ContractFunctionArgs<
-    abi,
-    'nonpayable' | 'payable',
-    functionName
-  >,
-  chainId extends config['chains'][number]['id'],
->(
-  variables: WriteContractVariables<abi, functionName, args, config, chainId> & {
-    paymaster: Address;
-    paymasterInput: Hex;
-  },
-  options?:
-    | MutateOptions<
-        WriteContractData,
-        WriteContractErrorType,
-        WriteContractVariables<
-          abi,
-          functionName,
-          args,
-          config,
-          chainId,
-          // use `functionName` to make sure it's not union of all possible function names
-          functionName
-        > & {
-          paymaster: Address;
-          paymasterInput: Hex;
-        },
-        context
-      >
-    | undefined,
-) => void;
-
-export type WriteContractSponsoredMutateAsync<
-  config extends Config,
-  context = unknown,
-> = <
-  const abi extends Abi | readonly unknown[],
-  functionName extends ContractFunctionName<abi, 'nonpayable' | 'payable'>,
-  args extends ContractFunctionArgs<
-    abi,
-    'nonpayable' | 'payable',
-    functionName
-  >,
-  chainId extends config['chains'][number]['id'],
->(
-  variables: WriteContractVariables<
-    abi,
-    functionName,
-    args,
-    config,
-    chainId
-  > & {
-    paymaster: Address;
-    paymasterInput: Hex;
-  },
-  options?:
-    | MutateOptions<
-        WriteContractData,
-        WriteContractErrorType,
-        WriteContractVariables<
-          abi,
-          functionName,
-          args,
-          config,
-          chainId,
-          // use `functionName` to make sure it's not union of all possible function names
-          functionName
-        > & {
-          paymaster: Address;
-          paymasterInput: Hex;
-        },
-        context
-      >
-    | undefined,
-) => Promise<WriteContractData>;
-
-type UseWriteContractSponsoredParameters<
-  config extends Config,
-  context,
-> = UseWriteContractParameters<config, context> & {
-  paymaster?: Address;
-  paymasterInput?: Hex;
-};
-
-export type UseWriteContractSponsoredReturnType<
-  config extends Config = Config,
-  context = unknown,
-> = UseMutationReturnType<
-  WriteContractData,
-  WriteContractErrorType,
-  WriteContractVariables<
-    Abi,
-    string,
-    readonly unknown[],
-    config,
-    config['chains'][number]['id']
-  > & {
-    paymaster: Address;
-    paymasterInput: Hex;
-  },
-  context
-> & {
-  writeContractSponsored: WriteContractSponsoredMutate<config, context>;
-  writeContractSponsoredAsync: WriteContractSponsoredMutateAsync<
-    config,
-    context
-  >;
-};
 
 export function useWriteContractSponsored<
   config extends Config = ResolvedRegister['config'],

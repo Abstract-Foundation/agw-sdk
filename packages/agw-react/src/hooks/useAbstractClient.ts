e3bfd514da6011cfc46e44559e7250ebd8b30d18
Coffee☕️ <coffee@coffeebot.dev>
2024-09-25 19:06:57 -0400

    add useAbstractClient hook

---
+++ packages/agw-react/src/hooks/useAbstractClient.ts
@@ -0,0 +1,28 @@
+import { createAbstractClient } from '@abstract-foundation/agw-client';
+import { useQuery } from '@tanstack/react-query';
+import { custom, useChains } from 'wagmi';
+
+import { useGlobalWalletSignerClient } from './useGlobalWalletSignerClient.js';
+
+export const useAbstractClient = () => {
+  const { data: signer } = useGlobalWalletSignerClient();
+  const [chain] = useChains();
+
+  return useQuery({
+    gcTime: 0,
+    queryKey: ['abstractClient'],
+    queryFn: () => {
+      if (!signer) {
+        throw new Error('No signer found');
+      }
+
+      const client = createAbstractClient({
+        signer: signer.account,
+        chain,
+        transport: custom(signer.transport),
+      });
+
+      return client;
+    },
+  });
+};

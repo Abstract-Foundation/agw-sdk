fbbcb6d68887070824594945217ecd0b5512452b
Coffee ☕️ <coffee@cubelabs.xyz>
2024-09-25 19:23:17 -0400

    add useAbstractClient hook (#37)

--- packages/agw-react/src/exports/index.ts
+++ packages/agw-react/src/exports/index.ts
@@ -1,4 +1,5 @@
 export { AbstractWalletProvider } from '../agwProvider.js';
+export { useAbstractClient } from '../hooks/useAbstractClient.js';
 export { useGlobalWalletSignerAccount } from '../hooks/useGlobalWalletSignerAccount.js';
 export { useGlobalWalletSignerClient } from '../hooks/useGlobalWalletSignerClient.js';
 export { useLoginWithAbstract } from '../hooks/useLoginWithAbstract.js';

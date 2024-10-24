5a749d482b21a8825b816544574c588a6149ac9d
Coffee ☕️ <coffee@cubelabs.xyz>
2024-10-24 16:29:33 -0400

    Create exported method for creating AGW accounts with no additional calldata (#64)
    * create account deployment action exported for creating smart accounts
    
    * typing
    
    * make initial signer optional; default to wallet client if not populated

---
+++ packages/agw-client/src/actions/deployAccount.ts
@@ -0,0 +1,89 @@
+import {
+  type Account,
+  type Address,
+  encodeFunctionData,
+  type Hash,
+  type Hex,
+  keccak256,
+  type PublicClient,
+  toBytes,
+  type Transport,
+  type WalletClient,
+  zeroAddress,
+} from 'viem';
+import type { ChainEIP712 } from 'viem/chains';
+
+import AccountFactoryAbi from '../abis/AccountFactory';
+import { SMART_ACCOUNT_FACTORY_ADDRESS, VALIDATOR_ADDRESS } from '../constants';
+import {
+  getInitializerCalldata,
+  getSmartAccountAddressFromInitialSigner,
+  isSmartAccountDeployed,
+} from '../utils';
+
+export interface DeployAccountParameters {
+  walletClient: WalletClient<Transport, ChainEIP712, Account>;
+  publicClient: PublicClient<Transport, ChainEIP712>;
+  initialSignerAddress?: Address;
+  paymaster?: Account;
+  paymasterInput?: Hex;
+}
+
+export interface DeployAccountReturnType {
+  smartAccountAddress: Address;
+  deploymentTransaction: Hash | undefined;
+}
+
+export async function deployAccount(
+  params: DeployAccountParameters,
+): Promise<DeployAccountReturnType> {
+  const {
+    initialSignerAddress,
+    walletClient,
+    publicClient,
+    paymaster,
+    paymasterInput,
+  } = params;
+
+  const initialSigner = initialSignerAddress ?? walletClient.account.address;
+
+  const address = await getSmartAccountAddressFromInitialSigner(
+    initialSigner,
+    publicClient,
+  );
+
+  let deploymentTransaction: Hash | undefined = undefined;
+
+  const isDeployed = await isSmartAccountDeployed(publicClient, address);
+  if (!isDeployed) {
+    const initializerCallData = getInitializerCalldata(
+      initialSigner,
+      VALIDATOR_ADDRESS,
+      {
+        allowFailure: false,
+        callData: '0x',
+        value: 0n,
+        target: zeroAddress,
+      },
+    );
+    const addressBytes = toBytes(initialSigner);
+    const salt = keccak256(addressBytes);
+    const deploymentCalldata = encodeFunctionData({
+      abi: AccountFactoryAbi,
+      functionName: 'deployAccount',
+      args: [salt, initializerCallData],
+    });
+
+    deploymentTransaction = await walletClient.sendTransaction({
+      to: SMART_ACCOUNT_FACTORY_ADDRESS,
+      data: deploymentCalldata,
+      paymaster,
+      paymasterInput,
+    });
+  }
+
+  return {
+    smartAccountAddress: address,
+    deploymentTransaction,
+  };
+}
--- packages/agw-client/src/exports/index.ts
+++ packages/agw-client/src/exports/index.ts
@@ -2,5 +2,6 @@ export {
   type AbstractClient,
   createAbstractClient,
 } from '../abstractClient.js';
+export { deployAccount } from '../actions/deployAccount.js';
 export { transformEIP1193Provider } from '../transformEIP1193Provider.js';
 export { getSmartAccountAddressFromInitialSigner } from '../utils.js';

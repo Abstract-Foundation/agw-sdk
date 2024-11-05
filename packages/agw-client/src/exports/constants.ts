3d24bee43fd6d8baf84bda1760862c6d262276e2
Coffee☕️ <coffee@coffeebot.dev>
2024-11-05 09:49:31 -0500

    export constants from agw-client

---
+++ packages/agw-client/src/exports/constants.ts
@@ -0,0 +1,11 @@
+import {
+  BATCH_CALLER_ADDRESS,
+  SMART_ACCOUNT_FACTORY_ADDRESS,
+  VALIDATOR_ADDRESS,
+} from '../constants.js';
+
+export {
+  BATCH_CALLER_ADDRESS as batchCallerAddress,
+  SMART_ACCOUNT_FACTORY_ADDRESS as smartAccountFactoryAddress,
+  VALIDATOR_ADDRESS as validatorAddress,
+};

3eb97c8edf07058fccb7080b032d3a7cbc204646
Part Thai <metarsit@outlook.com>
2024-11-13 21:02:56 +0700

    infra: Update CDN URL (#106)

--- packages/agw-react/src/constants.ts
+++ packages/agw-react/src/constants.ts
@@ -1,4 +1,4 @@
 const AGW_APP_ID = 'cm04asygd041fmry9zmcyn5o5';
-const ICON_URL = 'https://d9s2izusg5pvp.cloudfront.net/icon/light.png';
+const ICON_URL = 'https://abstract-assets.abs.xyz/icons/light.png';
 
 export { AGW_APP_ID, ICON_URL };

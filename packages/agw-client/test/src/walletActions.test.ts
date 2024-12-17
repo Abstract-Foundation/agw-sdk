afe839c04c9edf68b313bfe709d7c3fcbc2f1422
Coffee☕️ <coffee@coffeebot.dev>
2024-12-17 13:50:23 -0500

    wallet action tests

--- packages/agw-client/test/src/walletActions.test.ts
+++ packages/agw-client/test/src/walletActions.test.ts
@@ -1,12 +1,19 @@
+import { parseEther } from 'viem';
 import { describe, expect, it, vi } from 'vitest';
 
 import * as deployContractModule from '../../src/actions/deployContract.js';
 import * as prepareTransactionRequestModule from '../../src/actions/prepareTransaction.js';
 import * as sendTransactionModule from '../../src/actions/sendTransaction.js';
+import * as sendTransactionForSessionModule from '../../src/actions/sendTransactionForSession.js';
 import * as signTransactionModule from '../../src/actions/signTransaction.js';
 import * as writeContractModule from '../../src/actions/writeContract.js';
+import * as writeContractForSessionModule from '../../src/actions/writeContractForSession.js';
 import { EOA_VALIDATOR_ADDRESS } from '../../src/constants.js';
-import { globalWalletActions } from '../../src/walletActions.js';
+import { LimitType, SessionConfig } from '../../src/sessions.js';
+import {
+  globalWalletActions,
+  sessionWalletActions,
+} from '../../src/walletActions.js';
 import { address } from '../constants.js';
 
 // Mock the imported modules
@@ -15,7 +22,8 @@ vi.mock('../../src/actions/signTransaction');
 vi.mock('../../src/actions/deployContract');
 vi.mock('../../src/actions/writeContract');
 vi.mock('../../src/actions/prepareTransaction');
-
+vi.mock('../../src/actions/writeContractForSession');
+vi.mock('../../src/actions/sendTransactionForSession');
 describe('globalWalletActions', () => {
   const mockSignerClient = {
     account: {
@@ -131,6 +139,79 @@ describe('globalWalletActions', () => {
       mockSignerClient,
       mockPublicClient,
       mockArgs,
+    );
+  });
+});
+
+describe('sessionWalletActions', () => {
+  const mockSignerClient = {
+    account: {
+      address: address.sessionSignerAddress,
+    },
+  } as any;
+  const mockPublicClient = {} as any;
+  const mockClient = {
+    account: {
+      address: address.smartAccountAddress,
+    },
+  } as any;
+
+  const session: SessionConfig = {
+    signer: mockSignerClient.account.address,
+    expiresAt: BigInt(Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7),
+    feeLimit: {
+      limit: parseEther('1'),
+      limitType: LimitType.Lifetime,
+      period: 0n,
+    },
+    callPolicies: [],
+    transferPolicies: [],
+  };
+
+  const actions = sessionWalletActions(
+    mockSignerClient,
+    mockPublicClient,
+    session,
+  )(mockClient);
+
+  it('should return an object with all expected methods', () => {
+    expect(actions).toHaveProperty('writeContract');
+    expect(actions).toHaveProperty('sendTransaction');
+  });
+
+  it('should call sendTransactionForSession with correct arguments', async () => {
+    const mockArgs = {
+      to: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+      value: 100n,
+    };
+    await actions.sendTransaction(mockArgs as any);
+    expect(
+      sendTransactionForSessionModule.sendTransactionForSession,
+    ).toHaveBeenCalledWith(
+      mockClient,
+      mockSignerClient,
+      mockPublicClient,
+      mockArgs,
+      session,
+    );
+  });
+
+  it('should call writeContractForSession with correct arguments', async () => {
+    const mockArgs = {
+      address: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+      abi: [],
+      functionName: 'transfer',
+      args: [],
+    };
+    await actions.writeContract(mockArgs as any);
+    expect(
+      writeContractForSessionModule.writeContractForSession,
+    ).toHaveBeenCalledWith(
+      mockClient,
+      mockSignerClient,
+      mockPublicClient,
+      mockArgs,
+      session,
     );
   });
 });

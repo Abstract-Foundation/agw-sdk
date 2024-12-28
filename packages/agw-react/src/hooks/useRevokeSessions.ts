b46dddf705c471ad97f3e9a4e4a569de66b7a2d2
toozol <ashwinswaroop93@gmail.com>
2024-12-28 01:52:49 -0500

    Add hook to revoke sessions

---
+++ packages/agw-react/src/hooks/useRevokeSessions.ts
@@ -0,0 +1,58 @@
+import { sessionKeyValidatorAddress } from '@abstract-foundation/agw-client/constants';
+import { getSessionHash } from '@abstract-foundation/agw-client/dist/types/sessions';
+import {
+  type SessionConfig,
+  SessionKeyValidatorAbi,
+} from '@abstract-foundation/agw-client/sessions';
+import type { WriteContractParameters } from '@wagmi/core';
+import type { Address, Hash, Hex } from 'viem';
+import { useWriteContract } from 'wagmi';
+
+export type RevokeSessionsArgs = {
+  sessions: SessionConfig | Hash | (SessionConfig | Hash)[];
+  paymaster?: Address;
+  paymasterData?: Hex;
+} & Omit<WriteContractParameters, 'address' | 'abi' | 'functionName' | 'args'>;
+
+export const useRevokeSessions = () => {
+  const { writeContract, writeContractAsync, ...writeContractRest } =
+    useWriteContract();
+
+  return {
+    revokeSessions: (params: RevokeSessionsArgs) => {
+      const { sessions, ...rest } = params;
+      const sessionHashes = getSessionHashes(sessions);
+      writeContract({
+        address: sessionKeyValidatorAddress,
+        abi: SessionKeyValidatorAbi,
+        functionName: 'revokeKeys',
+        args: sessionHashes,
+        ...(rest as any),
+      });
+    },
+    revokeSessionsAsync: async (params: RevokeSessionsArgs) => {
+      const { sessions, ...rest } = params;
+      const sessionHashes = getSessionHashes(sessions);
+      await writeContractAsync({
+        address: sessionKeyValidatorAddress,
+        abi: SessionKeyValidatorAbi,
+        functionName: 'revokeKeys',
+        args: [sessionHashes],
+        ...(rest as any),
+      });
+    },
+    ...writeContractRest,
+  };
+};
+
+function getSessionHashes(
+  sessions: SessionConfig | Hash | (SessionConfig | Hash)[],
+): Hash[] {
+  return typeof sessions === 'string'
+    ? [sessions as Hash]
+    : Array.isArray(sessions)
+      ? sessions.map((session) =>
+          typeof session === 'string' ? session : getSessionHash(session),
+        )
+      : [getSessionHash(sessions)];
+}

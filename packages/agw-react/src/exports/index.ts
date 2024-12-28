e8bb8027f90b203288ceb874ce094f1812677edd
toozol <14188341+toozol@users.noreply.github.com>
2024-12-28 02:03:59 -0500

    Export hook

--- packages/agw-react/src/exports/index.ts
+++ packages/agw-react/src/exports/index.ts
@@ -4,4 +4,5 @@ export { useCreateSession } from '../hooks/useCreateSession.js';
 export { useGlobalWalletSignerAccount } from '../hooks/useGlobalWalletSignerAccount.js';
 export { useGlobalWalletSignerClient } from '../hooks/useGlobalWalletSignerClient.js';
 export { useLoginWithAbstract } from '../hooks/useLoginWithAbstract.js';
+export { useRevokeSessions } from '../hooks/useRevokeSessions.js';
 export { useWriteContractSponsored } from '../hooks/useWriteContractSponsored.js';

71f1005604b60bc0d2affc58395b97806cd32552
myuksal <33453423+myuksal@users.noreply.github.com>
2025-01-23 00:38:25 +0900

    feat: export abi constant (#188)

--- packages/agw-client/src/exports/constants.ts
+++ packages/agw-client/src/exports/constants.ts
@@ -1,4 +1,10 @@
+import AccountFactoryAbi from '../abis/AccountFactory.js';
 import AGWAccountAbi from '../abis/AGWAccount.js';
+import { AGWRegistryAbi } from '../abis/AGWRegistryAbi.js';
+import { BridgeHubAbi } from '../abis/BridgeHubAbi.js';
+import { DelegateRegistryAbi } from '../abis/DelegateRegistry.js';
+import { ExclusiveDelegateResolverAbi } from '../abis/ExclusiveDelegateResolver.js';
+import { ZkSyncAbi } from '../abis/ZkSyncAbi.js';
 import {
   AGW_REGISTRY_ADDRESS,
   BRIDGEHUB_ADDRESS,
@@ -8,14 +14,22 @@ import {
   SESSION_KEY_VALIDATOR_ADDRESS,
   SMART_ACCOUNT_FACTORY_ADDRESS,
 } from '../constants.js';
+import { SessionKeyValidatorAbi } from './sessions.js';
 
 export {
-  AGWAccountAbi as AGWAccountAbi,
+  AccountFactoryAbi,
+  AGWAccountAbi,
+  AGWRegistryAbi,
   AGW_REGISTRY_ADDRESS as agwRegistryAddress,
+  BridgeHubAbi,
   BRIDGEHUB_ADDRESS as bridgehubAddress,
+  DelegateRegistryAbi,
   CANONICAL_DELEGATE_REGISTRY_ADDRESS as delegateRegistryAddress,
+  ExclusiveDelegateResolverAbi,
   CANONICAL_EXCLUSIVE_DELEGATE_RESOLVER_ADDRESS as exclusiveDelegateResolverAddress,
+  SessionKeyValidatorAbi,
   SESSION_KEY_VALIDATOR_ADDRESS as sessionKeyValidatorAddress,
   SMART_ACCOUNT_FACTORY_ADDRESS as smartAccountFactoryAddress,
   EOA_VALIDATOR_ADDRESS as validatorAddress,
+  ZkSyncAbi,
 };

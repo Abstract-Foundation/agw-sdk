33565ed7da58e26cdb7baa006d10289038adf94c
Coffee☕️ <coffee@coffeebot.dev>
2025-01-21 21:36:00 -0500

    export actions

---
+++ packages/agw-client/src/exports/actions.ts
@@ -0,0 +1,4 @@
+export { deployAccount } from '../actions/deployAccount.js';
+export { getLinkedAccounts } from '../actions/getLinkedAccounts.js';
+export { getLinkedAgw } from '../actions/getLinkedAgw.js';
+export { linkToAgw } from '../actions/linkToAgw.js';

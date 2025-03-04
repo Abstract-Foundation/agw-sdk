3e58fdcbd95f415b0460d97bb253841f68e9c8f4
Coffee☕️ <coffee@coffeebot.dev>
2025-03-04 08:20:03 -0500

    fix: build

--- packages/agw-react/src/exports/privy.ts
+++ packages/agw-react/src/exports/privy.ts
@@ -4,4 +4,4 @@ export {
 } from '../privy/abstractPrivyProvider.js';
 export { InjectWagmiConnector } from '../privy/injectWagmiConnector.js';
 export { useAbstractPrivyLogin } from '../privy/useAbstractPrivyLogin.js';
-export { usePrivyCrossAppProvider } from "../privy/usePrivyCrossAppProvider.js';
+export { usePrivyCrossAppProvider } from '../privy/usePrivyCrossAppProvider.js';

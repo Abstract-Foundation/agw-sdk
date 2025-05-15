671a6f9462576265bcb91c6cb5b787b0b39528d9
Coffee ☕️ <coffee@cubelabs.xyz>
2025-05-15 16:10:18 -0400

    export get session status (#291)
    * export getSessionStatus
    
    * changeset

--- packages/agw-client/src/exports/actions.ts
+++ packages/agw-client/src/exports/actions.ts
@@ -1,5 +1,6 @@
 export { deployAccount } from '../actions/deployAccount.js';
 export { getLinkedAccounts } from '../actions/getLinkedAccounts.js';
 export { getLinkedAgw } from '../actions/getLinkedAgw.js';
+export { getSessionStatus } from '../actions/getSessionStatus.js';
 export { linkToAgw } from '../actions/linkToAgw.js';
 export { getBatchTransactionObject } from '../actions/sendTransactionBatch.js';

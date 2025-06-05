c9d79d26842fe572199404e14a4c42a9a1d23118
Coffee ☕️ <coffee@cubelabs.xyz>
2025-06-05 10:53:40 -0400

    feat: create minimal agw-web package that implements an eip6963 announcer (#316)

---
+++ packages/agw-web/src/exports/index.ts
@@ -0,0 +1,7 @@
+import { announceProvider } from '../eip6963emitter.js';
+
+export { announceProvider as initAbstractGlobalWallet };
+export type {
+  AnnounceProviderParameters,
+  AnnounceProviderReturnType,
+} from '../types.js';
